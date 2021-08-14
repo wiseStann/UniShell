5a0809f453e5e16d9d71e6f1aa2edc888ac0b910
Stacey Kerr <servicename18@gmail.com>
2021-08-15 00:50:15 +0300

    correct main loop

--- main.c
+++ main.c
@@ -22,15 +22,8 @@ int main(int argc, char** argv)
 
         cmd = command_parse_new(input->buffer);
         if (cmd) {
-            printf("Command name: %s\n", cmd->name);
-            printf("Arguments:\n");
-            for (int i = 0; i < cmd->args_num; i++) {
-                printf("Name: '%s', size: %d, index number: %d\n", cmd->arguments[i]->name, cmd->arguments[i]->size, cmd->arguments[i]->idx_number);
-            }
-            printf("\n");
-
             int status = command_handle(cmd);
-            if (!status)
+            if (status == -1)
                 printf("Invalid set of arguments was given to the command '%s'\n", cmd->name);
         }
 
