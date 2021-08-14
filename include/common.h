c1b720c6805b87faee401de84d00b19b929a4920
Stacey Kerr <servicename18@gmail.com>
2021-08-14 15:08:02 +0300

    added desc commentaries for sprecial characters

--- include/common.h
+++ include/common.h
@@ -40,63 +40,72 @@ typedef enum cmds_aliases {
 extern const char SPEC_SYMBOLS[SPEC_SYMBOLS_MAX_NUM];
 extern const unsigned int SPEC_SYMBOLS_NUM;
 
-//
+// checks if an array contains a given character
 int char_array_contains(const char*, char);
 
-//
+// checks if an array of commands contains a given command
 int commands_array_contains(avaliable_cmd_entry_t*, const char*);
 
-//
+// gets the index of a given command in an array
 int commands_array_get_index(avaliable_cmd_entry_t*, const char*);
 
 
 // specific symbols which can occur in a command 
 enum SYMBOL_KIND {
-    /* if the COMMENT_SYMBOL occurs, then all the following
+    /*  If COMMENT_SYMBOL occurs, then all the following
      * symbols are insignificant and we can stop parsing a command 
      */ 
     COMMENT_SYMBOL = '#',
 
-    /* if the SPACE_SYMBOL occurs, then we can stop parsing current
+    /* If SPACE_SYMBOL occurs, then we can stop parsing current
      * token and push it to the command arguments array
      */
     SPACE_SYMBOL = ' ',
 
-    /* if the QUOTE_SYMBOL occurs, then the "sentence token" follows up
+    /*  If QUOTE_SYMBOL occurs, then the "sentence token" follows up
      * or is already successfully read. if the "sentence token" follows up,
      * we start reading it and looking forward for the second QUOTE_SYMBOL.
-     * if the "sentence token" is already successfully read, we push it to
+     * If the "sentence token" is already successfully read, we push it to
      * the command arguments array
      */
     DOUBLE_QUOTE_SYMBOL = '\"',
 
-    /* 
-     *
+    /*  If BACK_SLASH_SYMBOL occurs and the newline character follows, then 
+     * it is treated as a line continuation (that is, it is removed from the
+     * input stream and effectively ignored). If there's no newline character
+     * following the BACK_SLASH_SYMBOL, then the exeception must be raised.
      */
     BACK_SLASH_SYMBOL = '\\',
 
-    /*
-     *
+    /*  FORWARD_SLASH_SYMBOL actually serves as a path separator. But there can
+     * be some exceptions.
      */
     FORWARD_SLASH_SYMBOL = '/',
 
-    /*
-     *
+    /*  If VERTICAL_BAR_SYMBOL occurs, then the "pipe" should be
+     * created between two processes. The first one takes the command part
+     * to VERTICAL_BAR_SYMBOL's left starting with the previous VERTICAL_BAR_SYMBOL,
+     * if it exists, or with the first character of the command, if it doesn't.
+     * The second one does in the mirror way, getting the commands to VERTICAL_BAR_SYMBOL's
+     * right. And so on, organizing something that's called a pipeline.
+     *  Example:
+     * shell>$ ps aux | grep [k]de | gawk '{ print $2}' 
      */
     VERTICAL_BAR_SYMBOL = '|',
 
-    /*
-     *
+    /*  If LESS_THAN_SYMBOL occurs, then the data from a given source should be
+     * pulled in a current stream.
      */
     LESS_THAN_SYMBOL = '<',
 
-    /*
-     *
+    /*  If a single GREATER_THAN_SYMBOL occurs, then the output streams
+     * shoud be redirected to some input stream and rewrite its contents.
+     * If a double GREATER_THAN_SYMBOL occurs, then the output streams
+     * should be redirected to some input stream and write after its contents.
      */
     GREATER_THAN_SYMBOL = '>',
 
-    /*
-     *
+    /*  If TERMINAL_SYMBOL occurs, then the end of the string (C string) is reached. 
      */
     TERMINAL_SYMBOL = '\0',
 };
@@ -105,8 +114,9 @@ enum SYMBOL_KIND {
 // custom allocation function
 void* sh_malloc(unsigned int bytes);
 
-//
+// shows available commands
 void commands_show();
+
 
 
 #endif // !SHELL_COMMON_H
