f99212d9c4091d6867ebfdf40d2b9dbec683bd9f
Stacey Kerr <servicename18@gmail.com>
2021-08-20 00:51:07 +0300

    add some code

--- src/sh_history.c
+++ src/sh_history.c
@@ -4,13 +4,25 @@
 /*
  *
  */
+his_entry_t* shell_history_entry_new(command_t* command)
+{
+    his_entry_t* entry = (his_entry_t*)sh_malloc(sizeof(his_entry_t));
+    entry->next = NULL;
+    entry->prev = NULL;
+    entry->command = command;
+
+    return entry;
+}
+
+/*
+ *
+ */
 history_t*
 shell_history_new()
 {
     history_t* history = (history_t*)sh_malloc(sizeof(history_t));
-    history->next = NULL;
-    history->prev = NULL;
-    history->command = (command_t*)sh_malloc(sizeof(command_t));
+    history->head = NULL;
+    history->size = 0;
 
     return history;
 }
@@ -19,10 +31,31 @@ shell_history_new()
  *
  */
 void
-shell_history_prepend(history_t* his, command_t* command)
+shell_history_prepend(history_t* history, command_t* command)
 {
-    if (!his)
-        his = shell_history_new();
-    his->command = command;
+    his_entry_t* entry = shell_history_entry_new(command);
+    if (!history->head)
+        history->head = entry;
+    else {
+        entry->next = history->head;
+        history->head->prev = entry;
+        history->head = entry;
+    }
+    history->size++;
+}
 
+/*
+ *
+ */
+void
+shell_history_free(history_t* history)
+{
+    his_entry_t* curr = history->head;
+    his_entry_t* tmp;
+    while (curr) {
+        tmp = curr->next;
+        free(curr);
+        curr = tmp;
+    }
+    free(history);
 }
