abcfeb01a9d5d74df90222793959aceb7ef5a441
Stacey Kerr <servicename18@gmail.com>
2021-08-26 00:56:19 +0300

    implement interaction with history (move up, move down)

--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "include/common.h"
-#include "include/parser.h"
 #include "include/input_buffer.h"
+#include "include/command.h"
+#include "include/parser.h"
 #include "include/sh_history.h"
+
 
 
 int main(int argc, char** argv)
@@ -10,65 +12,120 @@ int main(int argc, char** argv)
     command_t* cmd = NULL;
     history_t* history = shell_history_new();
     his_entry_t* next_entry;
-    int curr, non_canon_curr;
+    int should_free_prompt_base = 0, cursor_pos_in_command = 0, non_canon_curr;
+    unsigned previous_watched_command_len;
+    char* esc_seq = (char*)sh_malloc(ESC_SEQUENCE_MAX_LEN);
 
     set_prompt_basename(DEFAULT_PROMPT_BASENAME);
 
-    while (TRUE) {
+    while (TRUE)
+    {
         input = input_new();
+        write_prompt_basename();
 
         /*  If arrow up or arrow down were pressed, then some manipulations with
          * shell history should be made and the history info should be shown to a user.
          */
-        non_canon_curr = get_key_pressed();
-        if (non_canon_curr > 127) {
-            if (history->head) {
-                switch (non_canon_curr)
-                {
-                    case ARROW_UP:
-                        if (history->curr_watching->next) {
-                            history->curr_watching = history->curr_watching->next;
-                            write(0, history->curr_watching->command->content, history->curr_watching->command->length);
-                            goto history_edit;
+        while ((non_canon_curr = get_key_pressed()) != ENTER_CODE)
+        {
+            switch (non_canon_curr)
+            {
+                case ARROW_UP:
+                    if (history->curr_watching->next) {
+                        previous_watched_command_len = input->size;
+                        printf("\nCommand length: %d\n", previous_watched_command_len);
+                        
+                        history->curr_watching = history->curr_watching->next;
+                        char tmp[history->curr_watching->command->length + ESC_SEQUENCE_MAX_LEN];
+                        strcpy(tmp, history->curr_watching->command->content);
+
+                        if (previous_watched_command_len > 0) {
+                            sprintf(esc_seq, "\033[%dD", previous_watched_command_len);
+                            char_array_prepend(tmp, esc_seq);
                         }
-                        break;
-                    case ARROW_DOWN:
-                        if (history->curr_watching->prev) {
-                            history->curr_watching = history->curr_watching->prev;
-                            write(0, history->curr_watching->command->content, history->curr_watching->command->length);
-                            goto history_edit;
+                        write(1, tmp, history->curr_watching->command->length + strlen(esc_seq));
+                        input_buffer_pop_last_n(input, previous_watched_command_len);
+                        printf("\nPUSHING TO BUFFER STRING: %s\n", history->curr_watching->command->content);
+                        input_buffer_push_string(input, history->curr_watching->command->content, history->curr_watching->command->length);
+                        printf("\nBUFFER NOW %s\n", input->buffer);
+
+                        if (history->curr_watching->command->length < previous_watched_command_len) {
+                            for (int i = history->curr_watching->command->length; i < previous_watched_command_len; i++) {
+                                char space = SPACE_SYMBOL;
+                                write(1, &space, 1);
+                            }
+                            sprintf(esc_seq, "\033[%dD", previous_watched_command_len - history->curr_watching->command->length);
+                            write(1, esc_seq, strlen(esc_seq));
                         }
-                        break;
-                }
-            } else {
-                putc('\n', stdout);
-            }
-        } else {
-            putc(non_canon_curr, stdout);
-            input_buffer_push(input, non_canon_curr);
+                    }
+                    break;
+                case ARROW_DOWN:
+                    if (history->curr_watching->prev) {
+                        previous_watched_command_len = input->size;
+                        printf("\nCommand length: %d\n", previous_watched_command_len);
 
-history_edit:
-            while ((curr = getchar()) != ENTER_CODE) {
-                if (curr != BACK_SLASH_SYMBOL)
-                    input_buffer_push(input, curr);
-            }
-            input_buffer_push(input, '\0');
-            if (!strcmp(input->buffer, "exit")) break;
+                        history->curr_watching = history->curr_watching->prev;
+                        char tmp[history->curr_watching->command->length + ESC_SEQUENCE_MAX_LEN];
+                        strcpy(tmp, history->curr_watching->command->content);
 
-            cmd = command_parse_new(input->buffer);
-            if (cmd) {
-                int status = command_handle(cmd);
-                if (status == -1)
-                    printf("Invalid set of arguments was given to the command '%s'\n", cmd->name);
-                shell_history_prepend(history, cmd);
-                // shell_history_show(history);
-            }
+                        if (previous_watched_command_len > 0) {
+                            sprintf(esc_seq, "\033[%dD", previous_watched_command_len);
+                            char_array_prepend(tmp, esc_seq);
+                        }
+                        write(1, tmp, history->curr_watching->command->length + strlen(esc_seq));
+                        input_buffer_pop_last_n(input, previous_watched_command_len);
+                        printf("PUSHING TO BUFFER STRING: %s\n", history->curr_watching->command->content);
+                        input_buffer_push_string(input, history->curr_watching->command->content, history->curr_watching->command->length);
+                        printf("BUFFER NOW %s\n", input->buffer);
 
-            input_free(input);
+                        if (history->curr_watching->command->length < previous_watched_command_len) {
+                            for (int i = history->curr_watching->command->length; i < previous_watched_command_len; i++) {
+                                char space = SPACE_SYMBOL;
+                                write(1, &space, 1);
+                            }
+                            sprintf(esc_seq, "\033[%dD", previous_watched_command_len - history->curr_watching->command->length);
+                            write(1, esc_seq, strlen(esc_seq));
+                        }
+                    }
+                    break;
+                case BACKSPACE:
+                    if (input->size > 0) {
+                        write(1, "\033[1D \033[1D", strlen("\033[1D \033[1D"));
+                        input_buffer_pop(input);
+                    }
+                    break;
+                default:
+                    write(1, &non_canon_curr, 1);
+                    input_buffer_push(input, non_canon_curr);
+                    cursor_pos_in_command++;
+                    break;
+            }
         }
+        if (!input->size) { printf("\n"); continue; }
+
+        putchar(non_canon_curr);
+        input_buffer_push(input, '\0');
+
+        // check if hte prompt basename has been changed. If so, we should free memory that
+        // has been allocated for a new basename to avoid memory leaks
+        if (strcmp(prompt_basename, DEFAULT_PROMPT_BASENAME)) should_free_prompt_base = 1;
+        if (!strcmp(input->buffer, "exit")) break;
+
+        cmd = command_parse_new(input->buffer);
+        if (cmd) {
+            int status = command_handle(cmd);
+            if (status == -1)
+                printf("Invalid set of arguments was given to the command '%s'\n", cmd->name);
+            shell_history_prepend(history, cmd);
+            // shell_history_show(history);
+        }
+
+        input_free(input);
     }
     command_free(cmd);
     shell_history_free(history);
+
+    if (should_free_prompt_base) free(prompt_basename);
 
     return 0;
 }
