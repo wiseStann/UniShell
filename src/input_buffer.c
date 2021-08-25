a0bbffa61c0b0f1fb83d54fe74722e09b665c861
Stacey Kerr <servicename18@gmail.com>
2021-08-26 01:04:23 +0300

    implement popping and pushing functions

--- src/input_buffer.c
+++ src/input_buffer.c
@@ -24,8 +24,65 @@ input_buffer_push(input_t* input, char ch)
 {
     if (input->size >= input->capacity)
         input_buffer_reallocate(input, input->capacity * INPUT_BUFFER_CAP_EXP_VAL);
-    
+
     input->buffer[input->size++] = ch;
+}
+
+/*
+ *
+ */
+void input_buffer_push_at(input_t* input, char ch, unsigned int pos)
+{
+    // in order not to make redudant memmove
+    if (pos == input->buffer[input->size])
+        input_buffer_push(input, ch);
+    else {
+        if (input->size >= input->capacity)
+            input_buffer_reallocate(input, input->capacity * INPUT_BUFFER_CAP_EXP_VAL);
+        
+        size_t mem_block = (input->size - pos) * sizeof(char*);
+        memmove(
+            &(input->buffer[pos + 1]),
+            &(input->buffer[pos]),
+            mem_block
+        );
+
+        input->buffer[pos] = ch;
+        input->size++;
+    }
+}
+
+/*
+ *
+ */
+void input_buffer_push_string(input_t* input, char* string, unsigned int size)
+{
+    for (int i = 0; i < size; i++)
+        input_buffer_push(input, string[i]);
+}
+
+/*
+ *
+ */
+void input_buffer_pop(input_t* input)
+{
+    if (input->size > 0) {
+        memmove (
+            &(input->buffer[input->size - 1]),
+            &(input->buffer[input->size]),
+            sizeof(char*)
+        );
+        input->size--;
+    }
+}
+
+/*
+ *
+ */
+void input_buffer_pop_last_n(input_t* input, unsigned bytes)
+{
+    for (int i = 0; i < bytes; i++)
+        input_buffer_pop(input);
 }
 
 /*
@@ -44,6 +101,7 @@ input_buffer_reallocate(input_t* input, unsigned int bytes)
 void
 input_free(input_t* input)
 {
+    input->size = 0;
     free(input->buffer);
     free(input);
 }
