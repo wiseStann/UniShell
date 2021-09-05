74aa6ede75d919d0b4764adce03c007cb739f983
Stacey Kerr <servicename18@gmail.com>
2021-09-06 01:22:54 +0300

    add tail field to history struct, add declarations: history append, history get save filename

--- include/sh_history.h
+++ include/sh_history.h
@@ -16,6 +16,7 @@ typedef struct HistoryEntry {
 //
 typedef struct History {
     his_entry_t* head;
+    his_entry_t* tail;
     his_entry_t* curr_watching;
     unsigned size;
 } history_t;
@@ -28,6 +29,9 @@ his_entry_t* shell_history_entry_new(command_t*);
 history_t* shell_history_new();
 
 //
+void shell_history_append(history_t*, command_t*);
+
+//
 void shell_history_prepend(history_t*, command_t*);
 
 //
@@ -36,6 +40,8 @@ void shell_history_show(history_t*);
 //
 void shell_history_free(history_t*);
 
+//
+char* shell_history_get_save_filename(const char*);
 
 
 #endif // !HISTORY_SHELL_H
