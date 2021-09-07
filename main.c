65239d22dafc243e4bea0e5cdef7ef6783a2f811
Stacey Kerr <servicename18@gmail.com>
2021-09-08 00:32:20 +0300

    add pipes support through commands array passing to pipeline handlers

--- main.c
+++ main.c
@@ -4,20 +4,18 @@
 #include "include/parser.h"
 #include "include/sh_history.h"
 #include "include/utils.h"
-
+#include "include/pipeline.h"
 
 
 
 
 /*
-        SAVING SESSION HISTORY TO THE FILE
-        LOADING HISTORY FROM THE FILE BY A GIVEN ID
+        PIPES HANDLING
+        UNKNOWN COMMANDS HANDLING WITH EXECVP
+        WRITE CURSOR MOVEMENT MACROSES
+        STREAM REDIRECTION HANDLING
+        FIX EXIT, I THINK THE REASON IS OTHER PROCESSES CREATED IN HANDLERS 
 */
-
-
-
-
-
 
 
 
@@ -32,12 +30,12 @@ int main(int argc, char** argv)
 
     input_t* input = NULL;
     command_t* cmd = NULL;
-    his_entry_t* entry_to_retrieve = NULL;
+    pipe_cmds_t* pipeline_commands = NULL;
 
     int free_prompt_base = 0, free_hist_filename = 0;
     int cursor_pos_in_command = 0, non_canon_curr;
     unsigned previous_watched_command_len;
-    char* esc_seq = (char*)sh_malloc(ESC_SEQUENCE_MAX_LEN);
+    char* esc_seq = (char*)sh_malloc(ESC_SEQUENCE_MAX_LEN), *ptr;
 
     while (TRUE)
     {
@@ -156,23 +154,39 @@ int main(int argc, char** argv)
         if (!input->size) { printf("\n"); continue; }
 
         putchar(non_canon_curr);
+        input_buffer_push(input, '\0');
 
         // check if the prompt basename has been changed. If so, we should free memory that
         // has been allocated for a new basename to avoid memory leaks
         if (!strcmp(prompt_basename, DEFAULT_PROMPT_BASENAME)) free_prompt_base = 1;
         if (!strcmp(hist_file_name, DEFAULT_HIST_FILENAME)) free_hist_filename = 1;
-        if (!strcmp(input->buffer, "exit")) { input_free(input); break; }
+        if (!strcmp(input->buffer, "exit")) { break; }
 
-        input_buffer_push(input, '\0');
-        cmd = command_parse_new(input->buffer);
-        if (cmd) {
-            shell_history_prepend(history, cmd);
-            int status = command_handle(cmd);
+        cmd = command_parse_new(input->buffer, FALSE);
+        shell_history_prepend(history, cmd);
+
+        // if the whole command is valid then it is splitted by pipes
+        // and joined in a chain of subcommands
+        if (cmd->is_valid) {
+            char* buffer = strdup(input->buffer);
+            pipeline_commands = pipeline_commands_new();
+            do {
+                cmd = command_parse_new(buffer, TRUE);
+
+                ptr = strchr(buffer, VERTICAL_BAR_SYMBOL);
+                if (ptr) buffer = ptr + 1;
+                else buffer += strlen(buffer);
+
+                if (cmd->is_valid) {
+                    pipeline_commands_push(pipeline_commands, cmd);
+                }
+            } while (*buffer);
+            int status = pipeline_commands_handle(pipeline_commands);
+            pipeline_commands_free(pipeline_commands);
         }
-
         input_free(input);
     }
-    command_free(cmd);
+
     shell_history_free(history);
 
     if (free_prompt_base) free(prompt_basename);
