1041672b8819083c502e6538030e063478060119
Stacey Kerr <servicename18@gmail.com>
2021-09-08 00:42:28 +0300

    fix command_new for handlng pipes, implement command_length, command_trim and command_info functions, command_free is also fixed

--- src/command.c
+++ src/command.c
@@ -19,30 +19,29 @@ command_argument_new(unsigned int argument_index)
  *
  */
 command_t*
-command_new(const char* command)
+command_new(char* command, int pipes_visible)
 {
-    command_parse_is_valid(command);
-
     command_t* struct_command;
     argument_t* argument;
     unsigned int cmd_args_num;
     
     struct_command = (command_t*)sh_malloc(sizeof(command_t));
-    struct_command->name = (char*)sh_malloc(COMMAND_NAME_MAX_LEN);
-    struct_command->name = command_parse_get_basename(command);
 
-    struct_command->length = strlen(command);
+    command_trim(command);
+    struct_command->length = command_length(command, pipes_visible);
 
     // allocate len + ESC_SEQUENCE_MAX_LEN bytes in order to expand contents with paticular
     // escape sequence in the future
     struct_command->content = (char*)sh_malloc(struct_command->length + ESC_SEQUENCE_MAX_LEN);
-    strcpy(struct_command->content, command);
+    strcpy(struct_command->content, char_array_substring(command, 0, struct_command->length));
+
+    struct_command->name = (char*)sh_malloc(COMMAND_NAME_MAX_LEN);
+    struct_command->name = command_parse_get_basename(struct_command);
 
     struct_command->table_index = commands_array_get_index(AVAILABLE_COMMANDS, struct_command->name);
 
-    cmd_args_num = command_parse_arguments_number(command);
+    cmd_args_num = command_parse_arguments_number(struct_command);
     struct_command->args_num = cmd_args_num;
-
     if (cmd_args_num) {
         struct_command->arguments = (argument_t**)sh_malloc(sizeof(argument_t*));
         for (int i = 0; i < cmd_args_num; i++) {
@@ -50,7 +49,53 @@ command_new(const char* command)
         }
     }
 
+    struct_command->is_valid = command_parse_is_valid(struct_command);
+
     return struct_command;
+}
+
+/*
+ *
+ */
+unsigned command_length(const char* command, int pipes_visible)
+{
+    char* ptr, *substr;
+    unsigned length;
+
+    ptr = strchr(command, VERTICAL_BAR_SYMBOL);
+    if (!ptr || !pipes_visible)
+        return strlen(command);
+    
+    substr = char_array_substring(command, 0, ptr - command);
+    char_array_trim(substr);
+
+    length = strlen(substr);
+    free(substr);
+
+    return length;
+}
+
+/*
+ *
+ */
+void command_trim(char* command)
+{
+    char_array_trim(command);
+}
+
+/*
+ *
+ */
+void command_info(command_t* command)
+{
+    printf("command name: '%s'\n", command->name);
+    printf("command content: '%s'\n", command->content);
+    printf("command length = %d\n", command->length);
+    printf("command args num: %d\n", command->args_num);
+    for (int i = 0; i < command->args_num; i++) {
+        printf("Arg %d: '%s'\n", i, command->arguments[i]->name);
+    }
+    printf("\n");
 }
 
 /*
@@ -60,11 +105,9 @@ void
 command_free(command_t* command)
 {
     if (command) {
-        if (command->args_num) {
-            for (int i = 0; i < command->args_num; i++) 
-                free(command->arguments[i]);
-            free(command->arguments);
-        }
+        for (int i = 0; i < command->args_num; i++) 
+            free(command->arguments[i]);
+        free(command->arguments);
         free(command->name);
         free(command->content);
         free(command);
